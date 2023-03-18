b7fcac55bf5af6e8cb0578e565343bc342de1bb7
ono-hiroki <thisismymailaddress13771377@docomo.ne.jp>
2023-03-18 20:38:23 +0900

    オービットコントロールの追加

--- main.js
+++ main.js
@@ -1,4 +1,5 @@
 import * as THREE from 'three';
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js';
 
 console.log(THREE);
 const canvas = document.getElementById('canvas')
@@ -23,6 +24,30 @@ const renderer = new THREE.WebGLRenderer({canvas: canvas, antialias: true,});
 renderer.setSize(sizes.width, sizes.height);
 renderer.setPixelRatio(window.devicePixelRatio);
 
+// envimage
+const urls = [
+    './envimage/right.png',
+    './envimage/left.png',
+    './envimage/up.png',
+    './envimage/down.png',
+    './envimage/front.png',
+    './envimage/back.png',
+]
 
-renderer.render(scene, camera);
+const loader = new THREE.CubeTextureLoader();
+scene.background = loader.load(urls);
+
+// controls
+const controls = new OrbitControls(camera, canvas);
+controls.enableDamping = true;
+
+
+function animate(){
+    controls.update();
+    renderer.render(scene, camera);
+    window.requestAnimationFrame(animate);
+}
+
+animate();
+
 
